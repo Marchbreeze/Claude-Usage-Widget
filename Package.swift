f20c87638c448df5b609e8f6b4364222c6ec2a32
sangho <marchbreeze03@gmail.com>
2026-06-10 15:45:00 +0000

    chore: SwiftPM package scaffolding

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "ClaudeUsageWidget",
+    platforms: [.macOS(.v13)],
+    targets: [
+        .target(name: "UsageCore"),
+        .executableTarget(name: "ClaudeUsageWidget", dependencies: ["UsageCore"]),
+        .testTarget(name: "UsageCoreTests", dependencies: ["UsageCore"]),
+    ]
+)
